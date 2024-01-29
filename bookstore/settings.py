373dcd66392719e4ff1c23e1d24e7b2838e4953c
felekekinfe <felekekinfe@gmail.com>
2024-01-29 14:01:12 -0500

    commit

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -137,10 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 MEDIA_ROOT=BASE_DIR/'media'
 MEDIA_URL='/media/'
-STATIC_ROOT='BookShop/bookapp/static'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT=BASE_DIR/'staticfiles'
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
